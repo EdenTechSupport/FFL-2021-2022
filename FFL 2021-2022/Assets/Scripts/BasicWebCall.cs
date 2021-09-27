0df1bd0641965fc550bc54bfcf17d1a765b36be5
edenspijker <edenspijker@Macintoshs-MacBook-Pro-102.local>
2021-09-27 11:17:36 +0200

    commits gefixed

--- FFL 2021-2022/Assets/Scripts/BasicWebCall.cs
+++ FFL 2021-2022/Assets/Scripts/BasicWebCall.cs
@@ -6,21 +6,19 @@ using UnityEngine.UI;
 
 public class BasicWebCall : MonoBehaviour
 {
-    public Text messageText;
-    public InputField scoreToSend;
+    public string Bestelling;
 
     readonly string getURL = "https://ffl2021-2022.000webhostapp.com/UWR_Tut_Get.php";
     readonly string postURL = "https://ffl2021-2022.000webhostapp.com/UWR_Tut_Post.php";
 
     private void Start()
     {
-        messageText.text = "bestelling";
+        InvokeRepeating("GetScore", 0f, 2f);
     }
     
 
-    public void OnButtonGetScore()
+    public void GetScore()
     {
-        messageText.text = "Downloading data...";
         StartCoroutine(SimpleGetRequest());
     }
 
@@ -37,40 +35,10 @@ public class BasicWebCall : MonoBehaviour
 
         else
         {
-            messageText.text = www.downloadHandler.text;
-        }
-    }
-
-    public void OnButtonSendScore()
-    {
-        if (scoreToSend.text == string.Empty)
-        {
-            messageText.text = "Error: No high score to send.\nEnter a value in the input field.";
-        }
-        else
-        {
-            messageText.text = "Sending data...";
-            StartCoroutine(SimplePostRequest(scoreToSend.text));
-        }
-    }
-
-    IEnumerator SimplePostRequest(string curScore)
-    {
-        List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
-        wwwForm.Add(new MultipartFormDataSection("curScoreKey", curScore));
-
-        UnityWebRequest www = UnityWebRequest.Post(postURL, wwwForm);
-
-        yield return www.SendWebRequest();
-
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.LogError(www.error);
-        }
-
-        else
-        {
-            messageText.text = www.downloadHandler.text;
+            if(www.downloadHandler.text != Bestelling)
+            {
+                Bestelling = www.downloadHandler.text;
+            }    
         }
     }
 }
