cbbd92eb964f53c588125eb1707e3447ac724b5f
BigMostu <91014071+BigMostu@users.noreply.github.com>
2021-09-27 08:56:37 +0200

    Revert "darnken.meta"
    This reverts commit cf36ed09e55ab897c85f7c0490a58293413ac5d6.

--- FFL 2021-2022/Assets/Scripts/SetRequest.cs
+++ FFL 2021-2022/Assets/Scripts/SetRequest.cs
@@ -6,20 +6,18 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class SetRequest : MonoBehaviour
-{   public string scoreToSend;
+{
+    public Text messageText;
+    public InputField scoreToSend;
     string ButtonName;
     public List<string> ButtonMask = new List<string>();
- 
-    public List<string> BestellingList = new List<string>();
-
-    int i;
 
     readonly string getURL = "https://ffl2021-2022.000webhostapp.com/UWR_Tut_Get.php";
     readonly string postURL = "https://ffl2021-2022.000webhostapp.com/UWR_Tut_Post.php";
 
     private void Start()
     {
-
+        messageText.text = "bestelling";
     }
 
     void Update()
@@ -30,24 +28,45 @@ public class SetRequest : MonoBehaviour
 
     void m_ButtonName()
     {
-      i++;
-      ButtonName = EventSystem.current.currentSelectedGameObject.name; 
-      
-      EventSystem.current.currentSelectedGameObject.name = " ";
+      ButtonName = EventSystem.current.currentSelectedGameObject.name;
     }
-    public void OnButtonSendScore()
+    public void OnButtonGetScore()
     {
-        if (scoreToSend == string.Empty)
+        messageText.text = "Downloading data...";
+        StartCoroutine(SimpleGetRequest());
+    }
+
+    IEnumerator SimpleGetRequest()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(getURL);
+
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError)
         {
-            print("Geen Items");
+            Debug.LogError(www.error);
         }
+
         else
         {
-            StartCoroutine(SimplePostRequest(scoreToSend));
+            messageText.text = www.downloadHandler.text;
         }
     }
 
-    IEnumerator SimplePostRequest(string curScore) 
+    public void OnButtonSendScore()
+    {
+        if (scoreToSend.text == string.Empty)
+        {
+            messageText.text = "Error: No high score to send.\nEnter a value in the input field.";
+        }
+        else
+        {
+            messageText.text = "Sending data...";
+            StartCoroutine(SimplePostRequest(scoreToSend.text));
+        }
+    }
+
+    IEnumerator SimplePostRequest(string curScore)
     {
         List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
         wwwForm.Add(new MultipartFormDataSection("curScoreKey", curScore));
@@ -63,7 +82,7 @@ public class SetRequest : MonoBehaviour
 
         else
         {
-            print("Bestelt");
+            messageText.text = www.downloadHandler.text;
         }
     }
 }
