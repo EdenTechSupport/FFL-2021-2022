32684f4d37fc7115c3a7f3b4293b7012e2e85f81
edengamedev <72466167+edengamedev@users.noreply.github.com>
2021-09-28 23:57:35 +0200

    Bezorger en Bestellen in apart unity project gedaan

---
+++ FLL 2021-2022 Bezorger/Assets/Scripts/BasicWebCall.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class BasicWebCall : MonoBehaviour
+{
+    public string Bestelling;
+    public string[] BestellingArr;
+    public Text text;
+    readonly string getURL = "https://ffl2021-2022.000webhostapp.com/UWR_Tut_Get.php";
+    readonly string postURL = "https://ffl2021-2022.000webhostapp.com/UWR_Tut_Post.php";
+
+    private void Start()
+    {
+        text.text = "";
+        InvokeRepeating("GetScore", 0f, 2f);
+    }
+    
+
+    public void GetScore()
+    {
+        StartCoroutine(SimpleGetRequest());
+    }
+
+    IEnumerator SimpleGetRequest()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(getURL);
+
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        }
+
+        else
+        {
+            if(www.downloadHandler.text != Bestelling)
+            {
+                Bestelling = www.downloadHandler.text;
+                Split();
+            }    
+        }
+    }
+
+    private void Split()
+    {
+        BestellingArr = Bestelling.Substring(1).Split(':');
+        for(int i = 0;i<BestellingArr.Length;i++)
+        {
+            text.text += "\n" + BestellingArr[i];
+        }
+    }
+}
